bb121d4b690358d9b1c85da102e167d23660decc
ted-miller92 <ted.miller92@gmail.com>
2022-01-25 12:21:25 -0800

    Spherical distance calculator

---
+++ Ch4/src/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args){
+
+
+        public static String reverse(String phrase){
+            String phrase = "";
+            for (int i = 0; i < phrase.length(); i ++){
+                result = phrase.charAt(i) + result;
+            }
+            return result;
+        }
+        reverse("Hellow, world!");
+    }
+}
