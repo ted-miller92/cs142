b87194e04861362dd46f2ffa69fbfd5279f7e1f6
ted-miller92 <ted.miller92@gmail.com>
2022-01-31 16:38:31 -0800

    Jan 31

--- Chapter2/src/Loop.java
+++
@@ -1,7 +0,0 @@
-public class Loop {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++){
-            System.out.println(i + " squared = " + (i * i));
-        }
-    }
-}
