dae3a112860ba46cd552f54b88f01cbc739f8c02
ted-miller92 <ted.miller92@gmail.com>
2022-02-07 20:57:54 -0800

    Assignment 5

--- Ch5/src/GuessingGame.java
+++ Ch5/src/GuessingGame.java
@@ -3,12 +3,11 @@ Theodore Miller
 CS142 - Chris Denzler
 Assignment 5
 This program play a guessing game with the user
-A random number is assigned between 1 and 100,
+A random number is assigned between 1 and a given maximum,
 then the user guesses by entering an integer.
 Each time the user enters, the program indicates whether the guess is too low or too high,
-until the correct number is guessed
+until the correct number is guessed. The program also tracks and reports how many guesses are made.
 */
-
 
 import java.util.*; // for scanner
 
@@ -16,21 +15,27 @@ public class GuessingGame {
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
 
-        int correctNumber = assignNumber(100);
-
-        System.out.println("Guess a number between 0 and 100");
+        // assign a random number to be the correct number
+        int correctNumber = assignNumber(50);
 
         // initialize a guess variable
         int guess = -1;
+        int guessCount = 0;
 
         do {
+            // prompt for next guess
             System.out.print("Your guess: ");
+
             guess = console.nextInt();
+
+            // evaluate guess
             System.out.println(evaluateGuess(guess, correctNumber));
+            guessCount++;
         } while (guess != correctNumber);
 
-        System.out.println();
+        System.out.println("You guessed " + guessCount + " times.");
     }
+
     // following method determines if guess is too low/high/correct
     public static String evaluateGuess(int guess, int correctNumber){
         if (guess < correctNumber){
@@ -40,9 +45,13 @@ public class GuessingGame {
         }
         return "Correct!";
     }
+
     // following method returns a random number between 0 and a given argument
+    // also tells user to guess between 0 and int max
     public static int assignNumber(int max){
         int correctNumber = (int) Math.floor(Math.random() * max);
+        // prompt user for guess
+        System.out.println("Guess a number between 0 and " + max);
         return correctNumber;
     }
 }
